15da7b7c9eeb534a10d0a1d2108a5a14b2d86f87
Yanwei Ni <etni@microsoft.com>
2019-09-22 15:50:42 +0800

    init

---
+++ linux/ubuntu/1604/dotnetcore-sdk.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+################################################################################
+##  File:  dotnetcore-sdk.sh
+##  Team:  CI-Platform
+##  Desc:  Installs .NET Core SDK
+################################################################################
+# Source the helpers for use with the script
+source $HELPER_SCRIPTS/document.sh
+source $HELPER_SCRIPTS/apt.sh
+
+wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+apt-get install apt-transport-https
+apt-get update
+apt-get install dotnet-sdk-2.1
+apt-get install dotnet-sdk-2.2
---
+++ linux/ubuntu/build-agent.sh
@@ -0,0 +1,20 @@
+source $HELPER_SCRIPTS/document.sh
+source $HELPER_SCRIPTS/apt.sh
+source ./7-zip.sh
+source ./azcopy.sh
+source ./azure-cli.sh
+source ./azure-devops-cli.sh
+source ./1604/basic.sh
+source ./build-essential.sh
+source ./docker-compose.sh
+source ./docker-moby.sh
+source ./docker.sh
+# source ./git.sh
+# source ./1604/go.sh
+source ./hhvm.sh
+source ./java-tools.sh
+source ./1604/kubernetes-tools.sh
+source ./mysql.sh
+source ./nodejs.sh
+source ./1604/php.sh
+source ./1604/powershellcore.sh
